391a69ee3a529c53db7e711b3f25017a7295588c
ali-ahmed-05 <aliahmed.pluton@gmail.com>
2021-12-27 13:29:19 +0500

    address export

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,18 +4,19 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+const { json } = require("hardhat/internal/core/params/argumentTypes");
 
   // This is a script for deploying your contracts. You can adapt it to deploy
 // yours, or create new ones.
 async function main() {
   // This is just a convenience check
-  if (network.name === "hardhat") {
-    console.warn(
-      "You are trying to deploy a contract to the Hardhat Network, which" +
-        "gets automatically created and destroyed every time. Use the Hardhat" +
-        " option '--network localhost'"
-    );
-  }
+  // if (network.name === "hardhat") {
+  //   console.warn(
+  //     "You are trying to deploy a contract to the Hardhat Network, which" +
+  //       "gets automatically created and destroyed every time. Use the Hardhat" +
+  //       " option '--network localhost'"
+  //   );
+  // }
 
   // ethers is avaialble in the global scope
   const [deployer] = await ethers.getSigners();
@@ -66,13 +67,23 @@ function saveFrontendFiles(lasm,nftPreSale,nftPubSale,nft) {
   if (!fs.existsSync(contractsDir)) {
     fs.mkdirSync(contractsDir);
   }
-
-  fs.writeFileSync(
-    contractsDir + "/contract-address.json",
-    JSON.stringify({ LASM: lasm.address ,  nftPreSale: nftPreSale.address ,nftPubSale: nftPubSale.address ,nft: nft.address }
-      , undefined, 2)
+let config = `
+ const LASM_addr = "${lasm.address}"
+ const nftPreSale_addr = "${nftPreSale.address}"
+ const nftPubSale_addr = "${nftPubSale.address}"
+ const nft_addr = "${nft.address}"
+`
+let data =  JSON.stringify(config)
+fs.writeFileSync(
+  contractsDir + '/addresses.js', JSON.parse(data)
+  
+);
+  // fs.writeFileSync(
+  //   contractsDir + "/contract-address.json",
+  //   JSON.stringify({ LASM: lasm.address ,  nftPreSale: nftPreSale.address ,nftPubSale: nftPubSale.address ,nft: nft.address }
+  //     , undefined, 2)
     
-  );
+  // );
   // fs.writeFileSync(
   //   contractsDir + "/contract-address.json",
   //    JSON.stringify({ nftPreSale: nftPreSale.address }, undefined, 2)
@@ -92,10 +103,54 @@ function saveFrontendFiles(lasm,nftPreSale,nftPubSale,nft) {
   const managerArtifact = artifacts.readArtifactSync("Manager");
   const CrowdsaleArtifact = artifacts.readArtifactSync("Crowdsale");
 
+
+  let abi  = `const LASM  = ${JSON.stringify(LasmArtifact)}`
+
+  data = JSON.stringify(abi,null,2)
+
   fs.writeFileSync(
-    contractsDir + "/LASM.json",
-    JSON.stringify(LasmArtifact, null, 2)
+    contractsDir + '/LASM.js', JSON.parse(data)
+    
   );
+
+  abi  = `const NFTCrowdsale  = ${JSON.stringify(nftPreSaleArtifact)}`
+
+  data = JSON.stringify(abi,null,2)
+
+  fs.writeFileSync(
+    contractsDir + '/NFTCrowdsale.js', JSON.parse(data)
+    
+  );
+
+  abi  = `const NFT  = ${JSON.stringify(nftArtifact)}`
+
+  data = JSON.stringify(abi,null,2)
+
+  fs.writeFileSync(
+    contractsDir + '/NFT.js', JSON.parse(data)
+    
+  );
+  abi  = `const Manager  = ${JSON.stringify(managerArtifact)}`
+
+  data = JSON.stringify(abi,null,2)
+
+  fs.writeFileSync(
+    contractsDir + '/Manager.js', JSON.parse(data)
+    
+  );
+  abi  = `const Crowdsale  = ${JSON.stringify(CrowdsaleArtifact)}`
+
+  data = JSON.stringify(abi,null,2)
+
+  fs.writeFileSync(
+    contractsDir + '/Crowdsale.js', JSON.parse(data)
+    
+  );
+
+  // fs.writeFileSync(
+  //   contractsDir + "/LASM.json",
+  //   JSON.stringify(LasmArtifact, null, 2)
+  // );
   fs.writeFileSync(
     contractsDir + "/NFTCrowdsale.json",
     JSON.stringify(NFTCrowdsale, null, 2)
